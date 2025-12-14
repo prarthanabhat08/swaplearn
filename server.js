3c4470631ae20b6f534d95b9a9ebfdf6e9eba04a
chintubhat08 <chintubhat52@example.com>
2025-12-14 22:26:42 +0530

    Fix app.listen final

--- server.js
+++ server.js
@@ -161,6 +161,7 @@ app.get("/api/match/mutual", (req, res) => {
 // -----------------------------------------
 // START SERVER
 // -----------------------------------------
+// Start server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
